381d35d4a1a2feb9449021783ca110f5dcdd2320
NfoTECH <fortunenwachukwu@gmail.com>
2022-11-01 10:47:02 +0100

    clean code architecture employed in the controller class

--- src/main/java/com/fortunate/nwachukwu/bvnvalidator/controller/BvnController.java
+++ src/main/java/com/fortunate/nwachukwu/bvnvalidator/controller/BvnController.java
@@ -3,14 +3,14 @@ package com.fortunate.nwachukwu.bvnvalidator.controller;
 import com.fortunate.nwachukwu.bvnvalidator.dtos.BvnRequest;
 import com.fortunate.nwachukwu.bvnvalidator.dtos.BvnResponse;
 import com.fortunate.nwachukwu.bvnvalidator.service.BvnService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@AllArgsConstructor
 @RequestMapping("/bv-service")
 public class BvnController {
-    @Autowired
-    private BvnService bvnService;
+    private final BvnService bvnService;
     @PostMapping("/svalidate/wrapper")
     public BvnResponse postRequest(@RequestBody BvnRequest bvnRequest) {
         return bvnService.validateBvn(bvnRequest);
